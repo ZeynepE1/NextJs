1d6ee06270269d064dedcfe0112a4bd7582aab68
ZeynepE1 <8916zeynep@gmail.com>
2022-01-20 23:07:22 +0300

    1

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+function HomePage2() {
+    return <div>
+        <Link href='/'>
+            <a>Ana Sayfa</a>
+        </Link>
+    </div>
+  }
+  
+  export default HomePage2
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function HomePage() {
+    return <div>
+        <Link href='/about'>
+            <a>About</a>
+        </Link>
+    </div>
+  }
+  
+  export default HomePage
+
